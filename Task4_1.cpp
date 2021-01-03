0d3c431a0ae2b6080e0a81732bc267f85c0399b6
52hzNKL <74646637+52hzNKL@users.noreply.github.com>
2021-01-03 20:57:09 +0700

    Add files via upload

---
+++ Task4_1.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+	char a[100];
+	cout<<" Nhap xau ki tu ";
+	gets(a);
+	char b[]="hello";
+	
+	char *p1, *p2, *p3;
+    int i=0,j=0,key=0;
+    p1 = a;
+    p2 = b;
+	
+	
+	if(strlen(a)<strlen(b))
+	{
+		cout<<"No";
+	}
+	else
+	{
+	    for(i = 0; i<strlen(a); i++)
+        {
+            if(*p1 == *p2)
+            {
+                p3 = p1;
+                for(j = 0 ; j<strlen(b) ; j++)
+                {
+                    if(*p3 == *p2)
+                    {
+                        p3++;p2++;
+                    } 
+                    else
+                        break;
+                }
+                p2 = b;
+                if(j == strlen(b))
+                {
+                    key = 1;
+                    cout<<"Yes";
+                }
+            }
+        p1++; 
+        }
+    if(key==0)
+    {
+       cout<<"No";
+    }
+	}
+	return 0;
+}
